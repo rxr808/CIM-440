17e4182350fd5209916b98a66c8ce7e8d8d193d0
Rachel Rubi <rxr808@miami.edu>
2019-09-25 14:07:51 -0400

    updated correct code

--- class_examples/distanceHitzone/sketch.js
+++ class_examples/distanceHitzone/sketch.js
@@ -2,36 +2,42 @@ var hitX = 100;
 var hitY = 100;
 var threshold = 20;
 var hitDistance = 0;
+var changeBackground = "white"
 
 function setup() {
   // put setup code here
-  createCanvas(400,400)
+  createCanvas(400,400);
 }
-
 
 function draw() {
-//put drawing code here
-ellipse(hitX,hitY,threshold*2,threshold*2);
-hitDistance = dist(mouseX, mouseY, hitX, hitY);
-console.log("hitDistance " + hitDistance);
-if(hitDIstance <= threshold){
-  //do something, if hitDistance less than or equal to 20, 0-20
-fill(127);
-text("click me", hitX, hitY+30);
-}else{
-//if top is false, then execute code in else statement
-fill(255);
-}
-// our variable hitDistance ^
-
-//if(hitDistance > threshold){
-  //do something, if hitDistance is greater than not equal than 21, 21 to infinity
-//fill(255);
-//}
-function mousePressed (){
-  if (hitDistance <= threshold){
-    //do something
-    fill (127);
+  // put drawing code here
+  background(changeBackground);
+  ellipse(hitX,hitY,threshold*2,threshold*2);
+  hitDistance = dist(mouseX,mouseY,hitX,hitY);
+  console.log("hitDistance " + hitDistance);
+  //Hover state
+  if(hitDistance <= threshold){
+    //do something, if hitDistance less than or equal to 20, 0-20
+    fill(127);
+    text("click me", hitX,hitY+30);
+  }else{
+    //if top is false, then execute code in else statement
+    fill(255);
   }
+
+  // if(hitDistance > threshold){
+  //   //do something, if hitDistance greater than 21, 21 - infinity
+  //   fill(255);
+  // }
 }
+
+function mousePressed(){
+  //click state
+  if(hitDistance <= threshold){
+    //do something, if hitDistance less than or equal to 20, 0-20
+    changeBackground = "blue";
+  }else{
+    //if top is false, then execute code in else statement
+    changeBackground = "white";
+  }
 }
--- class_examples/rectHitzone/sketch.js
+++ class_examples/rectHitzone/sketch.js
@@ -11,15 +11,15 @@ function setup() {
 function draw() {
   // put drawing code here
   background(255);
-if(mouseX > hitX && mouseX < hitX + hitW && mouseY > hitY && mouse Y < hitY + hitH){
-  //if all conditions are true, do something;
-  console.log("In here!");
-  fill(255,0,0)
-  ellipse(100,100,10,10);
-}else{
-  //if i am not inside of the rectangle, print outside
-  console.log("Outside!");
-  fill(255, 255, 255);
-}
-rect(hitX,hitY,hitW,hitH);
+  if(mouseX > hitX && mouseX < hitX + hitW && mouseY > hitY && mouseY < hitY + hitH){
+    // if all conditions are true do something;
+    console.log("In here!");
+    fill(255,0,0);
+    ellipse(100,100,10,10);
+  }else{
+    //if i am not inside of the rectangle, print outside
+    console.log("Outside!");
+    fill(255,255,255);
+  }
+  rect(hitX,hitY,hitW,hitH);
 }
